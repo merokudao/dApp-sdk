488e418810a2431a690d94bd87573046c603aa35
AK46rocks <ak46rocks@gmail.com>
2023-07-26 15:53:53 +0530

    added types comments to analytics-dapplooker and analytics-ui-dapplooker

--- packages/analytics-ui-dapplooker/src/index.tsx
+++ packages/analytics-ui-dapplooker/src/index.tsx
@@ -1,3 +1,11 @@
 import {DLTableElement, DLTableReactElement} from 'dapplooker-visualizer-sdk';
 
+/**
+ * The `DLTableElement` interface represents a table element in the DappLooker Visualizer SDK.
+ * The `DLTableReactElement` interface represents a table element for ReactJs in the DappLooker Visualizer SDK.
+ *
+ * @export
+ * @interface DLTableElement
+ * @interface DLTableReactElement
+ */
 export {DLTableElement, DLTableReactElement};
